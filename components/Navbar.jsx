121142e8eb15fedcc2ca435d8de8e9a2f3415d45
Kristina Chausheva <k.chausheva@gmail.com>
2022-05-02 21:59:22 +0300

    restyling

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -22,7 +22,6 @@ function Navbar() {
           </Link>
           <li className={styles.listItem}>Products</li>
           <li className={styles.listItem}>Menu</li>
-          <Image src="/img/logo.png" alt="" width="160px" height="69px" />
           <li className={styles.listItem}>Events</li>
           <li className={styles.listItem}>Blog</li>
           <li className={styles.listItem}>Contact</li>
@@ -32,7 +31,7 @@ function Navbar() {
         <div className={styles.item}>
           <div className={styles.cart}>
             <Image src="/img/cart.png" alt="" width="30px" height="30px" />
-            <div className={styles.counter}>{}</div>
+            <div className={styles.counter}>{2}</div>
           </div>
         </div>
       </Link>
