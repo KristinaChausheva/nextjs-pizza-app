c1503ba28a849b20d636da64f0ccb66eea8be3a3
Kristina Chausheva <k.chausheva@gmail.com>
2022-05-03 21:05:57 +0300

    restyling

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -18,7 +18,7 @@ function Navbar() {
       <div className={styles.item}>
         <ul className={styles.list}>
           <Link href="/" passHref>
-            <li className={styles.listItem}>Homepage</li>
+            <li className={styles.listItem}>Home</li>
           </Link>
           <li className={styles.listItem}>Products</li>
           <li className={styles.listItem}>Menu</li>
