63e40253547046ab559d5a00a417abce56fa9da4
Kristina Chausheva <k.chausheva@gmail.com>
2022-05-04 22:46:22 +0300

    linking pizza page

--- components/PizzaCard.jsx
+++ components/PizzaCard.jsx
@@ -6,7 +6,10 @@ const PizzaCard = ({ pizza }) => {
   console.log(pizza)
   return (
     <div className={styles.container}>
-      <Image src={pizza.img} alt="" width="500" height="500" />
+      <Link href={`/product/${pizza._id}`}>
+        <Image src={pizza.img} alt="" width="500" height="500" />
+      </Link>
+
       <h1 className={styles.title}>{pizza.title}</h1>
       <span className={styles.price}>${pizza.prices[0]}</span>
       <p className={styles.desc}>{pizza.desc}</p>
